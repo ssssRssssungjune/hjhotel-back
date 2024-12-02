76d929077b8ec60beface36a2e37279933f13fd4
jelee <waftyann@gmail.com>
2024-12-02 16:17:46 +0900

    24.12.02 지은 : paypal 결제 구현에 필요한 MyBatisConfig.java 추가

---
+++ src/main/java/com/hjhotelback/config/MyBatisConfig.java
@@ -0,0 +1,34 @@
+package com.hjhotelback.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.hjhotelback.mapper")  // 매퍼 인터페이스가 있는 패키지 경로
+public class MyBatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        
+        // Mapper XML 위치 설정
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/**/*.xml"));
+        
+        // MyBatis 설정
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);  // snake_case를 camelCase로 자동 변환
+        sessionFactory.setConfiguration(configuration);
+        return sessionFactory.getObject();
+    }
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
