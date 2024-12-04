da97149dee8076d2ef3ef91d4cd2526923a3cd58
jelee <waftyann@gmail.com>
2024-12-04 17:41:55 +0900

    24.12.04 지은 : 결제 오류 수정 1차

--- src/main/java/com/hjhotelback/dto/payment/Order.java
+++ src/main/java/com/hjhotelback/dto/payment/Order.java
@@ -11,6 +11,7 @@ public class Order {
     private Integer id;					// 주문 고유 ID
     private String paypalOrderId;		// PayPal에서 받은 주문 ID
     private String status;				// 주문 상태 (PENDING, COMPLETED, FAILED, CANCELLED)
+//    private Integer paymentId;			// 결제 ID
     private Integer reservationId;		// 상품 ID (예: 예약된 객실 ID), reservationId(예약 ID)
     private BigDecimal amount;			// 결제 금액
     private LocalDateTime createdAt;	// 주문 생성 시간
