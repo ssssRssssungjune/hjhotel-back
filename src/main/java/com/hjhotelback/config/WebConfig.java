66b07a7154b085173ce5589348e7be8581aed4d2
an <ssungjune0610@gmail.com>
2024-12-05 16:19:07 +0900

    24.12.05 성준 : 쿠키가 보고싶었다

--- src/main/java/com/hjhotelback/config/WebConfig.java
+++ src/main/java/com/hjhotelback/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.hjhotelback.config;
 
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 쿠키 및 인증정보 허용
-                .allowedHeaders("*"); // 모든 헤더 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .exposedHeaders("Set-Cookie"); // 응답 헤더 노출
     }
 }
