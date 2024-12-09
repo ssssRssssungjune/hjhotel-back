9d69b2ca7a8d2ebd56407057bbef69166262075e
eziquexx <waftyann@gmail.com>
2024-12-10 07:10:29 +0900

    24.12.10 지은 : application.properties 수정 및 WEbConfig cherry-pick으로 해당 commit 가져와서 추가

---
+++ src/main/java/com/hjhotelback/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.hjhotelback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에 대해
+                .allowedOrigins("http://localhost:3000") // 허용할 프론트엔드 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키를 포함한 요청 허용
+    }
+}
