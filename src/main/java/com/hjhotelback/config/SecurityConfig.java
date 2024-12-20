815d734e960ed42ea8802e0a9cee0dc552bb8fe5
shinis(ys) <actionshin@gmail.com>
2024-12-20 14:25:28 +0900

    cors 정책 수정 외 소소한 수정

--- src/main/java/com/hjhotelback/config/SecurityConfig.java
+++ src/main/java/com/hjhotelback/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
