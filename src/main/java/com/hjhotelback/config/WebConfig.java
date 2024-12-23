520b8718396135fbf1dd41c0b227c29d172df0e3
Oonet96 <captaing96@gmail.com>
2024-12-23 20:58:53 +0900

    24.12.23 한택 프론트엔드 도메인 추가

--- src/main/java/com/hjhotelback/config/WebConfig.java
+++ src/main/java/com/hjhotelback/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "http://hotel-api") // 프론트엔드 도메인
+                .allowedOrigins("http://localhost:3000", "http://hotel-api", "http://cofile.co.kr") // 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 쿠키 및 인증정보 허용
                 .allowedHeaders("*") // 모든 헤더 허용
